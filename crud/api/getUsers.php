8c3188df55a3c124b37554e19c17ef043a14079f
UP210289 <up210289@alumnos.upa.edu.mx>
2024-04-10 16:24:29 -0600

    Funcionalidad de crud al 2%

---
+++ crud/api/getUsers.php
@@ -0,0 +1,21 @@
+<?php
+
+include "./partials/Connection.php";
+
+try {
+    $SQL = "SELECT * FROM user;";
+
+    $state = $conn -> query($SQL);
+
+    $json = [];
+    while ($row =  $state -> fetch(PDO::FETCH_ASSOC)) {
+        $json[] = [
+            "id" => $row['id'],
+            "fullname" => "{$row['firstname']} {$row['lastname']}"
+        ];
+    }
+
+    echo json_encode($json);
+} catch (\PDOException $e) {
+    die($e->getMessage());
+}
---
+++ crud/api/partials/Connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$dbName = "tasklist";
+$user = "root";
+$password = "";
+$protocol = "mysql:host={$host};dbname={$dbName}";
+try {
+  // Generación de la Conexion a la base de datos
+  $conn = new PDO($protocol, $user, $password);
+} catch (PDOException $e) {
+  die($e->getMessage());
+}
